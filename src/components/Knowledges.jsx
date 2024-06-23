4e888509d3ce18b56a674883e50eb3893ad76f6c
estebancastano <estebin5372@gmail.com>
2024-06-23 12:04:33 -0500

    Añadido sticky header y contenido de relleno

--- src/components/Knowledges.jsx
+++ src/components/Knowledges.jsx
@@ -2,7 +2,83 @@ import React from 'react'
 
 function Knowledges () {
   return (
-    <div>Knowledges</div>
+    <>
+            <p className='text-2xl'>
+                lorem wkoenopwneeeeeewfoejfe
+                eroierigjerogjoeirjeojojeodijcocs
+                csdovijdoivnonivodnvdnviodnfvndfonv
+            </p>
+            <p className='text-xl'>
+                lorem wkoenopwneeeeeewfoejfe
+                eroierigjerogjoeirjeojojeodijcocs
+                csdovijdoivnonivodnvdnviodnfvndfonv
+            </p>
+            <p className='text-xl'>
+                lorem wkoenopwneeeeeewfoejfe
+                eroierigjerogjoeirjeojojeodijcocs
+                csdovijdoivnonivodnvdnviodnfvndfonv
+            </p>
+            <p className='text-2xl'>
+                lorem wkoenopwneeeeeewfoejfe
+                eroierigjerogjoeirjeojojeodijcocs
+                csdovijdoivnonivodnvdnviodnfvndfonv
+            </p>
+            <p className='text-xl'>
+                lorem wkoenopwneeeeeewfoejfe
+                eroierigjerogjoeirjeojojeodijcocs
+                csdovijdoivnonivodnvdnviodnfvndfonv
+            </p>
+            <p className='text-xl'>
+                lorem wkoenopwneeeeeewfoejfe
+                eroierigjerogjoeirjeojojeodijcocs
+                csdovijdoivnonivodnvdnviodnfvndfonv
+            </p>
+            <p className='text-2xl'>
+                lorem wkoenopwneeeeeewfoejfe
+                eroierigjerogjoeirjeojojeodijcocs
+                csdovijdoivnonivodnvdnviodnfvndfonv
+            </p>
+            <p className='text-xl'>
+                lorem wkoenopwneeeeeewfoejfe
+                eroierigjerogjoeirjeojojeodijcocs
+                csdovijdoivnonivodnvdnviodnfvndfonv
+            </p>
+            <p className='text-xl'>
+                lorem wkoenopwneeeeeewfoejfe
+                eroierigjerogjoeirjeojojeodijcocs
+                csdovijdoivnonivodnvdnviodnfvndfonv
+            </p>
+            <p className='text-2xl'>
+                lorem wkoenopwneeeeeewfoejfe
+                eroierigjerogjoeirjeojojeodijcocs
+                csdovijdoivnonivodnvdnviodnfvndfonv
+            </p>
+            <p className='text-xl'>
+                lorem wkoenopwneeeeeewfoejfe
+                eroierigjerogjoeirjeojojeodijcocs
+                csdovijdoivnonivodnvdnviodnfvndfonv
+            </p>
+            <p className='text-xl'>
+                lorem wkoenopwneeeeeewfoejfe
+                eroierigjerogjoeirjeojojeodijcocs
+                csdovijdoivnonivodnvdnviodnfvndfonv
+            </p>
+            <p className='text-2xl'>
+                lorem wkoenopwneeeeeewfoejfe
+                eroierigjerogjoeirjeojojeodijcocs
+                csdovijdoivnonivodnvdnviodnfvndfonv
+            </p>
+            <p className='text-xl'>
+                lorem wkoenopwneeeeeewfoejfe
+                eroierigjerogjoeirjeojojeodijcocs
+                csdovijdoivnonivodnvdnviodnfvndfonv
+            </p>
+            <p className='text-xl'>
+                lorem wkoenopwneeeeeewfoejfe
+                eroierigjerogjoeirjeojojeodijcocs
+                csdovijdoivnonivodnvdnviodnfvndfonv
+            </p>
+        </>
   )
 }
 
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 function Portfolio() {
     return (
-        <div>Portfolio</div>
+        <>
+            <p className='text-2xl'>
+                lorem wkoenopwneeeeeewfoejfe
+                eroierigjerogjoeirjeojojeodijcocs
+                csdovijdoivnonivodnvdnviodnfvndfonv
+            </p>
+            <p className='text-xl'>
+                lorem wkoenopwneeeeeewfoejfe
+                eroierigjerogjoeirjeojojeodijcocs
+                csdovijdoivnonivodnvdnviodnfvndfonv
+            </p>
+            <p className='text-xl'>
+                lorem wkoenopwneeeeeewfoejfe
+                eroierigjerogjoeirjeojojeodijcocs
+                csdovijdoivnonivodnvdnviodnfvndfonv
+            </p>
+        </>
     )
 }
 
