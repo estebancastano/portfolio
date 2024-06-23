9af41dcc1d5352fbd8db377dc37f241e179ce538
estebancastano <estebin5372@gmail.com>
2024-06-23 11:36:51 -0500

    Añadido Header y creado los demas componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import AboutMe from "./components/AboutMe.jsx";
+import ContactMe from "./components/ContactMe.jsx";
 import Header from "./components/Header.jsx";
 import Hero from "./components/Hero.jsx"
+import Knowledges from "./components/Knowledges.jsx";
+import Portfolio from "./components/Portfolio.jsx";
 import "./index.css"
 
 export default function App() {
   return (
     <>
-    <Header />
-    <Hero />
+    <Header/>
+    <Hero/>
+    <AboutMe/>
+    <Knowledges/>
+    <Portfolio/>
+    <ContactMe/>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors:{'green':'#507255', 'beige':'#FEEFE5', 'green-light':'#7BAF82'}
+  },
+    
   },
   plugins: [],
 }
