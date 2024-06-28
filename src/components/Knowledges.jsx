cf6861ed5fee5582c9827ed5c49a6c1f462c3200
estebancastano <estebin5372@gmail.com>
2024-06-28 16:14:26 -0500

    mejorado hero, contactme y aboutme

--- src/components/Knowledges.jsx
+++ src/components/Knowledges.jsx
@@ -1,88 +1,87 @@
-import React from 'react'
+function Knowledges() {
+    return (
+        <>
+            <div id='skills' className="mb-16">
+                Conocimientos
 
-function Knowledges () {
-  return (
-    <>
-    <div id='skills'>
-    Conocimientos
-    </div>
-            <p className='text-2xl'>
-                lorem wkoenopwneeeeeewfoejfe
-                eroierigjerogjoeirjeojojeodijcocs
-                csdovijdoivnonivodnvdnviodnfvndfonv
-            </p>
-            <p className='text-xl'>
-                lorem wkoenopwneeeeeewfoejfe
-                eroierigjerogjoeirjeojojeodijcocs
-                csdovijdoivnonivodnvdnviodnfvndfonv
-            </p>
-            <p className='text-xl'>
-                lorem wkoenopwneeeeeewfoejfe
-                eroierigjerogjoeirjeojojeodijcocs
-                csdovijdoivnonivodnvdnviodnfvndfonv
-            </p>
-            <p className='text-2xl'>
-                lorem wkoenopwneeeeeewfoejfe
-                eroierigjerogjoeirjeojojeodijcocs
-                csdovijdoivnonivodnvdnviodnfvndfonv
-            </p>
-            <p className='text-xl'>
-                lorem wkoenopwneeeeeewfoejfe
-                eroierigjerogjoeirjeojojeodijcocs
-                csdovijdoivnonivodnvdnviodnfvndfonv
-            </p>
-            <p className='text-xl'>
-                lorem wkoenopwneeeeeewfoejfe
-                eroierigjerogjoeirjeojojeodijcocs
-                csdovijdoivnonivodnvdnviodnfvndfonv
-            </p>
-            <p className='text-2xl'>
-                lorem wkoenopwneeeeeewfoejfe
-                eroierigjerogjoeirjeojojeodijcocs
-                csdovijdoivnonivodnvdnviodnfvndfonv
-            </p>
-            <p className='text-xl'>
-                lorem wkoenopwneeeeeewfoejfe
-                eroierigjerogjoeirjeojojeodijcocs
-                csdovijdoivnonivodnvdnviodnfvndfonv
-            </p>
-            <p className='text-xl'>
-                lorem wkoenopwneeeeeewfoejfe
-                eroierigjerogjoeirjeojojeodijcocs
-                csdovijdoivnonivodnvdnviodnfvndfonv
-            </p>
-            <p className='text-2xl'>
-                lorem wkoenopwneeeeeewfoejfe
-                eroierigjerogjoeirjeojojeodijcocs
-                csdovijdoivnonivodnvdnviodnfvndfonv
-            </p>
-            <p className='text-xl'>
-                lorem wkoenopwneeeeeewfoejfe
-                eroierigjerogjoeirjeojojeodijcocs
-                csdovijdoivnonivodnvdnviodnfvndfonv
-            </p>
-            <p className='text-xl'>
-                lorem wkoenopwneeeeeewfoejfe
-                eroierigjerogjoeirjeojojeodijcocs
-                csdovijdoivnonivodnvdnviodnfvndfonv
-            </p>
-            <p className='text-2xl'>
-                lorem wkoenopwneeeeeewfoejfe
-                eroierigjerogjoeirjeojojeodijcocs
-                csdovijdoivnonivodnvdnviodnfvndfonv
-            </p>
-            <p className='text-xl'>
-                lorem wkoenopwneeeeeewfoejfe
-                eroierigjerogjoeirjeojojeodijcocs
-                csdovijdoivnonivodnvdnviodnfvndfonv
-            </p>
-            <p className='text-xl'>
-                lorem wkoenopwneeeeeewfoejfe
-                eroierigjerogjoeirjeojojeodijcocs
-                csdovijdoivnonivodnvdnviodnfvndfonv
-            </p>
+                <p className='text-2xl'>
+                    lorem wkoenopwneeeeeewfoejfe
+                    eroierigjerogjoeirjeojojeodijcocs
+                    csdovijdoivnonivodnvdnviodnfvndfonv
+                </p>
+                <p className='text-xl'>
+                    lorem wkoenopwneeeeeewfoejfe
+                    eroierigjerogjoeirjeojojeodijcocs
+                    csdovijdoivnonivodnvdnviodnfvndfonv
+                </p>
+                <p className='text-xl'>
+                    lorem wkoenopwneeeeeewfoejfe
+                    eroierigjerogjoeirjeojojeodijcocs
+                    csdovijdoivnonivodnvdnviodnfvndfonv
+                </p>
+                <p className='text-2xl'>
+                    lorem wkoenopwneeeeeewfoejfe
+                    eroierigjerogjoeirjeojojeodijcocs
+                    csdovijdoivnonivodnvdnviodnfvndfonv
+                </p>
+                <p className='text-xl'>
+                    lorem wkoenopwneeeeeewfoejfe
+                    eroierigjerogjoeirjeojojeodijcocs
+                    csdovijdoivnonivodnvdnviodnfvndfonv
+                </p>
+                <p className='text-xl'>
+                    lorem wkoenopwneeeeeewfoejfe
+                    eroierigjerogjoeirjeojojeodijcocs
+                    csdovijdoivnonivodnvdnviodnfvndfonv
+                </p>
+                <p className='text-2xl'>
+                    lorem wkoenopwneeeeeewfoejfe
+                    eroierigjerogjoeirjeojojeodijcocs
+                    csdovijdoivnonivodnvdnviodnfvndfonv
+                </p>
+                <p className='text-xl'>
+                    lorem wkoenopwneeeeeewfoejfe
+                    eroierigjerogjoeirjeojojeodijcocs
+                    csdovijdoivnonivodnvdnviodnfvndfonv
+                </p>
+                <p className='text-xl'>
+                    lorem wkoenopwneeeeeewfoejfe
+                    eroierigjerogjoeirjeojojeodijcocs
+                    csdovijdoivnonivodnvdnviodnfvndfonv
+                </p>
+                <p className='text-2xl'>
+                    lorem wkoenopwneeeeeewfoejfe
+                    eroierigjerogjoeirjeojojeodijcocs
+                    csdovijdoivnonivodnvdnviodnfvndfonv
+                </p>
+                <p className='text-xl'>
+                    lorem wkoenopwneeeeeewfoejfe
+                    eroierigjerogjoeirjeojojeodijcocs
+                    csdovijdoivnonivodnvdnviodnfvndfonv
+                </p>
+                <p className='text-xl'>
+                    lorem wkoenopwneeeeeewfoejfe
+                    eroierigjerogjoeirjeojojeodijcocs
+                    csdovijdoivnonivodnvdnviodnfvndfonv
+                </p>
+                <p className='text-2xl'>
+                    lorem wkoenopwneeeeeewfoejfe
+                    eroierigjerogjoeirjeojojeodijcocs
+                    csdovijdoivnonivodnvdnviodnfvndfonv
+                </p>
+                <p className='text-xl'>
+                    lorem wkoenopwneeeeeewfoejfe
+                    eroierigjerogjoeirjeojojeodijcocs
+                    csdovijdoivnonivodnvdnviodnfvndfonv
+                </p>
+                <p className='text-xl'>
+                    lorem wkoenopwneeeeeewfoejfe
+                    eroierigjerogjoeirjeojojeodijcocs
+                    csdovijdoivnonivodnvdnviodnfvndfonv
+                </p>
+            </div>
         </>
-  )
+    )
 }
 
 export default Knowledges
