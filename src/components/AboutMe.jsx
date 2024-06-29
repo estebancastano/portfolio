154a0c84a51b3673903c4dabdc66879ef1ebcd31
estebancastano <estebin5372@gmail.com>
2024-06-29 12:07:51 -0500

    Agregado info a aboutme

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -13,9 +13,11 @@ function AboutMe() {
                 <div className='flex text-white h-60 bg-green-light '>
                     <img src={Profile} className='flex rounded-full ml-32 items-center mb-5' />
 
-                    <div className='flex flex-col m-10 sm:flex-wrap'>
+                    <div className='flex m-10 sm:flex-wrap'>
                         <h2 className='text-2xl md:text-3xl'>Sobre mi</h2>
-                        <p className='text-xl md:text-2xl h-auto max-w-full'>Soy apadisohe ghergiehrgoie heirhgierhg eigheir ejkkebg  </p>
+                        <p className='text-xl md:text-2xl h-auto max-w-full'>Soy un programador web con un año de experiencia en tecnologías como <strong>HTML, CSS, React, Tailwind y JavaScript</strong>. También sé trabajar con <strong>Git</strong>. Aunque estoy en las etapas iniciales de mi carrera y todavía tengo mucho que aprender, soy una persona proactiva que siempre busca mejorar. Trabajo bien en equipo y me esfuerzo por ser amable y respetuoso con mis compañeros.
+
+                        </p>
                     </div>
 
 
