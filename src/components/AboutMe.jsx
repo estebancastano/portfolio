136aad391eee9e492a0d12c7952f6108ee3ab598
estebancastano <estebin5372@gmail.com>
2024-06-30 14:39:47 -0500

    organizado la informacion aboutme

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,24 +5,23 @@ import Wave from '../assets/wave.svg'
 function AboutMe() {
     return (
         <>
-            <div className='mb-16 -mt-52'>
-                <div id='about' className='h-30 about-me'>
-                    <img src={Wave} />
-
+            <div className='mb-16 sm:-mt-52 md:-mt-52 -mt-40'>
+                <div id='about' className='h-auto about-me'>
+                    <img src={Wave} alt='Wave' className='w-full' />
                 </div>
-                <div className='flex text-white h-60 bg-green-light '>
-                    <img src={Profile} className='flex rounded-full ml-32 items-center mb-5' />
-
-                    <div className='flex m-10 sm:flex-wrap'>
+                <div className='relative flex flex-col md:flex-row text-white bg-green-light h-auto md:h-60 z-10'>
+                    <div className='flex justify-center items-center mt-[-6rem] md:mt-0'>
+                        <img src={Profile} alt='Profile' className='rounded-full w-32 md:w-48' />
+                    </div>
+                    <div className='flex flex-col justify-center md:ml-20 ml-8 mt-4 md:mt-0'>
                         <h2 className='text-2xl md:text-3xl'>Sobre mi</h2>
-                        <p className='text-xl md:text-2xl h-auto max-w-full'>Soy un programador web con un año de experiencia en tecnologías como <strong>HTML, CSS, React, Tailwind y JavaScript</strong>. También sé trabajar con <strong>Git</strong>. Aunque estoy en las etapas iniciales de mi carrera y todavía tengo mucho que aprender, soy una persona proactiva que siempre busca mejorar. Trabajo bien en equipo y me esfuerzo por ser amable y respetuoso con mis compañeros.
-
+                        <p className='text-xl md:text-2xl h-auto max-w-full'>
+                            Soy Esteban Andres Castaño, programador web con un año de experiencia en tecnologías como <strong>HTML, CSS, React, Tailwind y JavaScript</strong>. También sé trabajar con <strong>Git</strong>. Aunque estoy en las etapas iniciales de mi carrera y todavía tengo mucho que aprender, soy una persona proactiva que siempre busca mejorar. Trabajo bien en equipo y me esfuerzo por ser amable y respetuoso con mis compañeros.
                         </p>
                     </div>
-
-
                 </div>
             </div>
+
         </>
     )
 }
