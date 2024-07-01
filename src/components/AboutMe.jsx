3b6ad4c3c0c9bdd757022766feddb63f283e58fd
estebancastano <estebin5372@gmail.com>
2024-07-01 10:56:06 -0500

    arreglado un poco el responsive del aboutme

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -11,9 +11,9 @@ function AboutMe() {
                 </div>
                 <div className='relative flex flex-col md:flex-row text-white bg-green-light h-auto md:h-60 z-10'>
                     <div className='flex justify-center items-center mt-[-6rem] md:mt-0'>
-                        <img src={Profile} alt='Profile' className='rounded-full w-32 md:w-48' />
+                        <img src={Profile} alt='Profile' className='flex rounded-full w-40 md:w-72 justify-center md:ml-14' />
                     </div>
-                    <div className='flex flex-col justify-center md:ml-20 ml-8 mt-4 md:mt-0'>
+                    <div className='flex flex-col justify-center md:ml-20 mt-4 md:mt-0 ml-0 md:max-w-5xl max-w-9xl'>
                         <h2 className='text-2xl md:text-3xl'>Sobre mi</h2>
                         <p className='text-xl md:text-2xl h-auto max-w-full'>
                             Soy Esteban Andres Castaño, programador web con un año de experiencia en tecnologías como <strong>HTML, CSS, React, Tailwind y JavaScript</strong>. También sé trabajar con <strong>Git</strong>. Aunque estoy en las etapas iniciales de mi carrera y todavía tengo mucho que aprender, soy una persona proactiva que siempre busca mejorar. Trabajo bien en equipo y me esfuerzo por ser amable y respetuoso con mis compañeros.
